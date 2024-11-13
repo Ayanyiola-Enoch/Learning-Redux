5971c778e6b34d3589239c2d37ace786f05f444f
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-11-13 17:53:29 +0100

    inserted git ignore

--- class/test.js
+++ class/test.js
@@ -1,5 +1,5 @@
 const redux = require('redux');
-// const createStore = redux.createStore();
+const createStore = redux.createStore;
 
 //INITIAL STATE
 const initialState = {
@@ -12,8 +12,48 @@ const initialState = {
 //ACTION CREATOR
 const orderCake = () => {
     return {
-        type: "dudu"
+        type: "enter"
+    };
+};
+const orderCake2 = () => {
+    return {
+        type: "move"
+    }
+}
+
+
+
+//REDUCER
+const cakeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "enter":
+            {/*FIRST CASE- REMOVING CAKE*/ }
+            return {
+                ...state,
+                cake: state.cake - 1
+            };
+        case "move":
+            {/*FIRST CASE- ADD CAKE*/ }
+            return {
+                ...state,
+                cake: state.cake + 1
+            }
+        default:
+            return state;
     };
 };
 
-//REDUCER
+//STORE
+const store = createStore(cakeReducer);
+
+console.log('enoch initial store', store.getState());
+
+store.subscribe(() => {
+    console.log('for checking', store.getState());
+})
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+store.dispatch(orderCake2());
