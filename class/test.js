a4f093bd89b418207b479b7564919d3666f2b8d0
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-11-13 15:36:50 +0100

    first commit

---
+++ class/test.js
@@ -0,0 +1,10 @@
+const redux = require('redux');
+// const createStore = redux.createStore();
+
+//INITIAL STATE
+const initialState = {
+    cake: 10,
+    email: "",
+    firstName: "",
+    todo: [],
+};
